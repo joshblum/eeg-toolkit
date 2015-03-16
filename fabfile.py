193faf6a1c277b8a673dc070657b4784043b1aed
Joshua Blum <joshblum@mit.edu>
2015-03-16 18:48:54 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,7 @@ def prod():
 def deploy():
   with cd(env.server_path):
     run('git pull --rebase origin master')
+    run('source ~/.bash_profile')
     run('workon eeg-spectrogram')
     run('make install')
     run('make prod-run')
