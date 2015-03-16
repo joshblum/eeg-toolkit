0ae30916e268d0aef762e6a2493daa3af698997b
Joshua Blum <joshblum@mit.edu>
2015-03-16 18:54:44 +0200

    correct host

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ def prod():
   env.user = 'joshblum'
   env.gateway = 'tre.ef.technion.ac.il'
   env.forward_agent = True
-  env.hosts = ['gpu-os02.ef.technion.ac.il']
+  env.hosts = ['gpu-os05.ef.technion.ac.il']
   env.key_filename = '~/.ssh/id_rsa.pub'
   env.server_path = '~/eeg-spectrogram'
   env.python_path = '~/.virtualenvs/eeg-spectrogram'
@@ -15,5 +15,6 @@ def prod():
 def deploy():
   with cd(env.server_path):
     run('git pull --rebase origin master')
+    run('workon eeg-spectrogram')
     run('make install')
     run('make prod-run')
