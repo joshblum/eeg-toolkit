18f556e757dacafd0c133ce0c4959b1e79c9f000
Joshua Blum <joshblum@mit.edu>
2015-10-29 18:03:31 -0400

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,18 @@
 from fabric.api import env, cd, run
 
+PROJ_NAME = 'eeg-toolkit'
 
 def prod():
   env.user = 'ubuntu'
   env.hosts = ['128.52.175.133', '128.52.178.57']
   env.key_filename = '~/.ssh/id_rsa.pub'
-  env.server_path = '~/eeg-spectrogram'
-  env.python_path = '~/.virtualenvs/eeg-spectrogram'
+  env.server_path = '~/%s' % PROJ_NAME
+  env.python_path = '~/.virtualenvs/%s' % PROJ_NAME
   return
 
 
 def deploy():
   with cd(env.server_path):
     run('git pull --rebase origin master')
-    run('workon eeg-spectrogram; make install')
-    run('workon eeg-spectrogram; make prod-run')
+    run('workon %s; make install' % PROJ_NAME)
+    run('workon %s; make prod-run' % PROJ_NAME)
